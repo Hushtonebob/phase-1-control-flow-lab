4e44ab53d60520314c3c7196aa9619ceac96fd8c
BobbyDeluna <bobby.deluna2@gmail.com>
2022-02-15 14:45:03 -0700

    Completed Assignment

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue){
+  let result
+    if (someValue <= 400) {
+      result = `This one is on me!`;}
+  
+    if (someValue > 2000){
+      result = `I will gladly take your thirty bucks.`;}
+
+    if (someValue > 2500){
+      result = `No can do.`;}
+
+  return result
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  let blam1 = city === `NYC` ? `Ok, sounds good.` : `No go.`;
+
+return blam1;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(tip){
+  
+  let tipResponse;
+  
+    switch (tip) { 
+      case `generous`:
+         tipResponse; return `Thank you so much.`;
+
+      case `not as generous`:
+        tipResponse; return `Thank you.`;
+       
+      default:
+        tipResponse; return `Bye.`;
+    }
+  }
